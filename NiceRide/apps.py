6e28ee82b369ee3c68d4f8a0b6e068901982b6db
Maksymilianx <79858139+Maksymilianx@users.noreply.github.com>
2021-06-07 20:53:17 +0200

    add accounts

---
+++ NiceRide/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NicerideConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'NiceRide'
---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from accounts import views
+
+urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
+]
