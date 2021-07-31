198c8f09eab8a3c61fba3167f7d167709cd59417
Maksymilianx <79858139+Maksymilianx@users.noreply.github.com>
2021-07-31 14:51:44 +0200

    add admin.py

--- NiceRide/admin.py
+++ NiceRide/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Car, OfferImage
+
+
+class OfferImageAdmin(admin.StackedInline):
+    model = OfferImage
+
+@admin.register(Car)
+class OfferAdmin(admin.ModelAdmin):
+    inlines = [OfferImageAdmin]
+
+    class Meta:
+        model = Car
+
+@admin.register(OfferImage)
+class OfferImageAdmin(admin.ModelAdmin):
+    pass
